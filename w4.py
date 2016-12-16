0817357a3b83aac3f87b85e7f7cf9b97a29d706d
s40523119 <40523119@gm.nfu.edu.tw>
2016-12-16 14:38:00 +0800

    123

--- w4.py
+++ w4.py
@@ -1,8 +1,10 @@
 '''
-mystring = "40523119"
-mystring += "test"
-print("mystring")
+mystring = "scrum-1444"
+print(mystring)
 '''
+
 thetext = input("Enter some text ")
-print ("This is what you entered:")
+print ("This is what you entered:", "test", sep="---")
 print (thetext)
+
+
